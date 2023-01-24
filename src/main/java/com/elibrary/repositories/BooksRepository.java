6b34462c2ea6ce5622a2ab5735c15adc9d98297a
Nikita Nasevich <nasevich.nik@mail.ru>
2023-01-24 11:27:04 +0300

    create PeopleService and BookService, refactor-BooksRepository

--- src/main/java/com/elibrary/repositories/BooksRepository.java
+++ src/main/java/com/elibrary/repositories/BooksRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public abstract class BooksRepository implements JpaRepository<Book, Integer> {
+public interface BooksRepository extends JpaRepository<Book, Integer> {
 }
